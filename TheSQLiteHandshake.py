be5ac857fa9ba97d8985571f78f3a56eb5631f30
GFC-01 <mhlopenkosikhona21@gmail.com>
2026-04-07 04:53:44 +0200

    Q2 refactor

--- TheSQLiteHandshake.py
+++ TheSQLiteHandshake.py
@@ -1,55 +1,42 @@
 import sqlite3
 
 db_connection = sqlite3.connect('school.db')
-
 my_cursor = db_connection.cursor()
-
 my_cursor.execute('CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY, name TEXT, grade INTEGER)')
 
-
 def add_student(name, grade):
-
-    my_cursor.execute("INSERT INTO students (name, grade) VALUES (?, ?)", (name, grade))
-
+    my_cursor.execute("INSERT INTO students (name, grade) VALUES (?, ?)", (name, grade)) # parametized query
     db_connection.commit()
 
 
 def print_students():
-
     my_cursor.execute("SELECT * FROM students")
-
     rows = my_cursor.fetchall()
-
-    for row in rows: print(row)
-
+    if not rows: print("There are no students yet!")
+    else:
+        for row in rows: print(row)
 
 def print_above_fifty():
-
     my_cursor.execute("SELECT * FROM students WHERE grade > 50")
     my_rows = my_cursor.fetchall()
-
-    for row in my_rows: print(row)
-
+    if not my_rows: print("There are no students yet!")
+    else:
+        for row in my_rows: print(row)
 
 user_response = input("Enter another student? (y/n): ")
 
-
 while user_response == 'y':
-
     student_name = input("Student Name: ")
-
-    student_grade = input("Student Grade: ")
-
+    try:
+        student_grade = int(input("Student Grade: "))
+    except Exception as e:
+        print(f"There was a {type(e).__name__} error!")
+        print(f"Error description: {e}")
     add_student(student_name, student_grade)
-
     user_response = input("Enter another student? (y/n): ")
 
-
 else:
-
     print("\n~~~~~~~~~~~~~~~~~~~ALL STUDENTS~~~~~~~~~~~~~~~~~~")
     print_students()
-
     print("\n~~~~~~~~~~~~~~~~~~~ABOVE 50~~~~~~~~~~~~~~~~~~~~~~~")
-
     print_above_fifty()

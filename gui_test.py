b2bf12da45f2dc282fc1f8445c1e22aa220a70f1
GFC-01 <mhlopenkosikhona21@gmail.com>
2026-04-07 05:21:51 +0200

    Q3 refactor

--- gui_test.py
+++ gui_test.py
@@ -5,19 +5,19 @@ app = customtkinter.CTk()
 app.geometry("400x180")
 app.title("SQL PRacticeTool - Prototype")
 
+def execute_sql():
+    inputText = sqlTextbox.get("1.0", "end-1c")
+    print(f"Executing: {inputText}")
 
-def execute_sql(sql):
-    return 0
+app.grid_columnconfigure(0, weight=1)
 
-# app.columnconfigure(0, 1)
-
-txtSql = customtkinter.CTkTextbox(app)
-query = txtSql.get("1.0", "end-1c") # start from line 1 character 0; got to the very end but ignore the invisible 'enter' key at the last character
-txtSql.grid(row=0, column=0, padx=10, pady=10)
-btnExecute = customtkinter.CTkButton(app, text="Run Query", command=execute_sql(query))
+sqlTextbox = customtkinter.CTkTextbox(app)
+sqlTextbox.grid(row=0, column=0, padx=10, pady=10)
+btnExecute = customtkinter.CTkButton(app, text="Run Query", command=execute_sql)
 btnExecute.grid(row=1, column=0, padx= 10, pady=10,)
 lblResult = customtkinter.CTkLabel(app, text="Result Area")
 lblResult.grid(row=2, column=0, padx=10, pady=10)
 
 app.mainloop()
+
 

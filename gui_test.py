e64a1a125f4addcbc41eba1a051e91a166431d5c
GFC-01 <mhlopenkosikhona21@gmail.com>
2026-04-07 02:53:32 +0200

    updated corrections

--- gui_test.py
+++ gui_test.py
@@ -12,7 +12,7 @@ def execute_sql(sql):
 # app.columnconfigure(0, 1)
 
 txtSql = customtkinter.CTkTextbox(app)
-query = txtSql.get("0.0", "end")
+query = txtSql.get("1.0", "end-1c") # start from line 1 character 0; got to the very end but ignore the invisible 'enter' key at the last character
 txtSql.grid(row=0, column=0, padx=10, pady=10)
 btnExecute = customtkinter.CTkButton(app, text="Run Query", command=execute_sql(query))
 btnExecute.grid(row=1, column=0, padx= 10, pady=10,)
